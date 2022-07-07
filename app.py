65fa7f67990b3bbf979dd237d1520c0ee77a7eb2
Anuvab Chakravarty <106730134+Vanaub22@users.noreply.github.com>
2022-07-07 22:17:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ import joblib
 model_nb=joblib.load('spam-ham')
 st.title('SPAM-HAM CLASSIFIER') #creates a title with name spam-ham classifier
 ip=st.text_input('Enter the Message:') #creates a text box
-op=model.predict([ip])
+op=model_nb.predict([ip])
 if st.button('Predict'):
   st.title(op[0]) #prints the output as spam or ham
