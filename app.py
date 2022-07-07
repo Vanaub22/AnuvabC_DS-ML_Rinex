332f81cb90bdeed879ee0d93227da49e5b9eea83
Anuvab Chakravarty <106730134+Vanaub22@users.noreply.github.com>
2022-07-07 22:08:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import joblib
-model=joblib.load('spam-ham')
+model_nb=joblib.load('spam-ham')
 st.title('SPAM-HAM CLASSIFIER') #creates a title with name spam-ham classifier
 ip=st.text_input('Enter the Message:') #creates a text box
 op=model.predict([ip])
 if st.button('Predict'):
-  st.title(op[0])
+  st.title(op[0]) #prints the output as spam or ham
     
