427b299c0579b553b8e054aed8c0c167e1a01a2b
Anuvab Chakravarty <106730134+Vanaub22@users.noreply.github.com>
2022-07-07 17:24:45 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import joblib
+model=joblib.load('spam-ham')
+st.title('SPAM-HAM CLASSIFIER') #creates a title with name spam-ham classifier
+ip=st.text_input('Enter the Message:') #creates a text box
+op=model.predict([ip])
+if st.button('Predict'):
+  st.title(op[0])
